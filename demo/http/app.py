f9a834e3021a29608ee8c62584e370c5bf546af1
Leon <Liangz.org@gmail.com>
2020-08-16 22:15:26 +0800

    feat(http): make http redirect is safe

--- demo/http/app.py
+++ demo/http/app.py
@@ -14,6 +14,7 @@
 from flask import (
     Flask, redirect, url_for, abort, make_response, request, session
 )
+from urllib.parse import urlparse, urljoin
 import os
 
 app = Flask(__name__)
@@ -103,9 +104,17 @@ def do_something():
 
 def request_back(defult='hello', **kwargs):
     for target in request.args.get('next'), request.referrer:
-        if target:
+        if not target:
+            continue
+        if is_safe_url(target):
             return redirect(target)
     return redirect(url_for(defult, **kwargs))
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
 
 
 if __name__ == '__main__':
