f8e697d95155f53941732d3f9e3603c39978833b
Leon <Liangz.org@gmail.com>
2020-08-17 23:14:15 +0800

    feat(template): user define 404 error

--- demo/template/app.py
+++ demo/template/app.py
@@ -100,5 +100,10 @@ def just_flash():
     return redirect(url_for('index'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
