e8a7eb7359647fc4bdc669b04b26af7bd77e28da
Leon <Liangz.org@gmail.com>
2020-08-25 22:51:54 +0800

    feat: add method to register blueprint and extensions

--- blog/__init__.py
+++ blog/__init__.py
@@ -19,7 +19,7 @@ from flask import Flask
 from blog.blueprints.admin import admin_bp
 from blog.blueprints.auth import auth_bp
 from blog.blueprints.blog import blog_bp
-
+from blog.extensions import bootstrap, db, ckeditor, mail, moment, migrate
 from blog.settings import config
 
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -30,10 +30,27 @@ def create_app(config_name=None):
         config_name = os.getenv('FLASK_CONFIG', 'development')
     app = Flask('Blog')
     app.config.from_object(config[config_name])
+
+    register_blueprints(app)
+
+    register_extensions(app)
+
+    return app
+
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    migrate.init_app(app, db)
+
+
+def register_blueprints(app):
     app.register_blueprint(blog_bp)
     app.register_blueprint(admin_bp, url_prefix='/admin')
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    return app
 
 
 if __name__ == '__main__':
