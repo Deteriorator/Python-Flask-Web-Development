5e04e5a3fd7c70df376e7642d2e6f1bf726ca852
Leon <Liangz.org@gmail.com>
2020-08-26 23:50:30 +0800

    fix: make blog app run normal

---
+++ blogapp/extensions.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------------------------------------
+   @Name:     extensions.py
+   @Desc:
+   @Author:   liangz.org@gmail.com
+   @Create:   2020.08.25   22:42
+-------------------------------------------------------------------------------
+   @Change:   2020.08.25
+-------------------------------------------------------------------------------
+"""
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+ckeditor = CKEditor()
+mail = Mail()
+moment = Moment()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from blogapp.models import Admin
+    user = Admin.query.get(int(user_id))
+    return user
+
+
+login_manager.login_view = 'auth.login'
+# login_manager.login_message = 'Your custom message'
+login_manager.login_message_category = 'warning'
