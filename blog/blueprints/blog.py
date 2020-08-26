c28b7e113b71f2866e3816760a7cb63638e20f1f
Leon <Liangz.org@gmail.com>
2020-08-26 22:10:36 +0800

    feat: switch theme

--- blog/blueprints/blog.py
+++ blog/blueprints/blog.py
@@ -11,12 +11,13 @@
 -------------------------------------------------------------------------------
 """
 
-from flask import Blueprint, render_template, current_app, request, redirect, url_for, flash
+from flask import Blueprint, render_template, current_app, request, redirect, url_for, flash, abort, make_response
 
 from blog.extensions import db
 from blog.emails import send_new_reply_email, send_new_comment_email
 from blog.forms import AdminCommentForm, CommentForm
 from blog.models import Post, Comment, Category
+from blog.utils import redirect_back
 
 blog_bp = Blueprint('blog', __name__)
 
@@ -108,3 +109,13 @@ def reply_comment(comment_id):
     return redirect(
         url_for('.show_post', post_id=comment.post_id, reply=comment_id,
                 author=comment.author) + '#comment-form')
+
+
+@blog_bp.route('/change-theme/<theme_name>')
+def change_theme(theme_name):
+    if theme_name not in current_app.config['BLUELOG_THEMES'].keys():
+        abort(404)
+
+    response = make_response(redirect_back())
+    response.set_cookie('theme', theme_name, max_age=30 * 24 * 60 * 60)
+    return response
---
+++ blog/utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------------------------------------
+   @Name:     utils.py
+   @Desc:     
+   @Author:   liangz.org@gmail.com
+   @Create:   2020.08.26   22:04
+-------------------------------------------------------------------------------
+   @Change:   2020.08.26
+-------------------------------------------------------------------------------
+"""
+
+try:
+    from urlparse import urlparse, urljoin
+except ImportError:
+    from urllib.parse import urlparse, urljoin
+
+from flask import request, redirect, url_for
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+def redirect_back(default='blog.index', **kwargs):
+    for target in request.args.get('next'), request.referrer:
+        if not target:
+            continue
+        if is_safe_url(target):
+            return redirect(target)
+    return redirect(url_for(default, **kwargs))
