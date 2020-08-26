c952a495095a74b25694793b815586cb56e4d04d
Leon <Liangz.org@gmail.com>
2020-08-26 09:46:36 +0800

    feat(blog): add email function

--- blog/settings.py
+++ blog/settings.py
@@ -42,9 +42,9 @@ class BaseConfig(object):
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = ('Bluelog Admin', MAIL_USERNAME)
+    MAIL_DEFAULT_SENDER = ('Blog Admin', MAIL_USERNAME)
 
-    BLUELOG_EMAIL = os.getenv('BLUELOG_EMAIL')
+    BLUELOG_EMAIL = os.getenv('Blog_EMAIL')
     BLUELOG_POST_PER_PAGE = 10
     BLUELOG_MANAGE_POST_PER_PAGE = 15
     BLUELOG_COMMENT_PER_PAGE = 15
